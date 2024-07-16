a3a618d40a0f55cbce40cd37a7e1c7b0621e8ac6
youssef1969 <youssefkatif@gmail.com>
2024-07-16 11:39:19 +0200

    add option zoom dans graphe et supp de zoom aussi

--- front/src/DataChart.js
+++ front/src/DataChart.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import zoomPlugin from 'chartjs-plugin-zoom';
 import './App.css';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, zoomPlugin);
 
 const DataChart = ({ data, tValues, label, axisLabel }) => {
+  const chartRef = useRef(null);
+
   const chartData = {
     labels: tValues,
     datasets: [
@@ -20,6 +23,28 @@ const DataChart = ({ data, tValues, label, axisLabel }) => {
   };
 
   const options = {
+    responsive: true,
+    plugins: {
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: 'xy',
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: 'xy',
+        },
+      },
+      title: {
+        display: true,
+        text: ``,
+      },
+    },
     scales: {
       x: {
         title: {
@@ -36,7 +61,18 @@ const DataChart = ({ data, tValues, label, axisLabel }) => {
     },
   };
 
-  return <Line data={chartData} options={options} />;
+  const handleResetZoom = () => {
+    if (chartRef.current) {
+      chartRef.current.resetZoom();
+    }
+  };
+
+  return (
+    <div>
+      <Line ref={chartRef} data={chartData} options={options} />
+      <button onClick={handleResetZoom}>Réinitialiser zoom</button>
+    </div>
+  );
 };
 
 export default DataChart;
